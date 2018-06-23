38763d70a8e87bd4100104d42adfd6392f7e86d2
alan19031216 <terry1903@live.com>
2018-06-23 13:43:35 +0800

    delete a Lines

--- php/login.php
+++ php/login.php
@@ -5,7 +5,6 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $conn->quote($username);
 $salt = '}#f4ga~g%7hjg4&j(7mk?/!bj30ab-wi=6^7-$^R9F|GK5J#E6WT;IO[JN'; // random string
-
 $password = sha1(md5($salt.$password)); // sha1 hash with salt & md5 #6
 
 try {
