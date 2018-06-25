ef5db4bbddbf75f746a95d23d37cf3f450285fbe
alan19031216 <terry1903@live.com>
2018-06-25 17:58:49 +0800

    check the user already order or not

--- page/success.php
+++ page/success.php
@@ -2,24 +2,32 @@
 include 'php/config.php';
 include 'html_php/new_hearder.php';
 //session_start();
- $username = $_SESSION['username'];
-$time = $_SESSION['$times'] ;
-$current_time = $_SESSION['$current_time'];
-$expired_date = $_SESSION['$expired_date'];
+$username = $_SESSION['username'];
+$sql_check = $conn->query("SELECT * FROM subscript WHERE username = '$username'");
+$count_row = $sql_check->rowCount();
+if($count_row == 0){
+  $time = $_SESSION['$times'] ;
+  $current_time = $_SESSION['$current_time'];
+  $expired_date = $_SESSION['$expired_date'];
 
-//echo $expired_date;
-try {
-	$sql = $conn->query("INSERT INTO subscript(username , times , subscript_date , expired_date)
-	VALUES ('$username' , '$time' , '$current_time' , '$expired_date')");
+  //echo $expired_date;
+  try {
+  	$sql = $conn->query("INSERT INTO subscript(username , times , subscript_date , expired_date)
+  	VALUES ('$username' , '$time' , '$current_time' , '$expired_date')");
 
-	$sql_history = $conn->query("INSERT INTO subs_history
-		(username , times , subscript_date , expired_date ,status) VALUES
-		('$username' , '$time', '$current_time' , '$expired_date' , 'active')");
-    //echo "Success";
+  	$sql_history = $conn->query("INSERT INTO subs_history
+  		(username , times , subscript_date , expired_date ,status) VALUES
+  		('$username' , '$time', '$current_time' , '$expired_date' , 'active')");
+      //echo "Success";
+  }
+  catch(PDOException $e) {
+  		echo "Error: " . $e->getMessage();
+  }
 }
-catch(PDOException $e) {
-		echo "Error: " . $e->getMessage();
+else{
+  echo "<script>window.location = 'new_home.php'</script>";  
 }
+
 
 ?>
  		<title>Order success</title>
