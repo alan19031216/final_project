0039975393b5695b0e1f116816aadd68e570429f
alan19031216 <terry1903@live.com>
2018-06-25 16:20:17 +0800

    edit the price bar and edit js

--- page/subscript.php
+++ page/subscript.php
@@ -59,7 +59,7 @@ include 'html_php/new_hearder.php';
              </p>
            </td>
            <td>
-             <a onClick="changeColour('a')"><h4>RM100</h4>
+             <a onClick="changeColour('a')"><h4>RM235</h4>
              <p>1 year of the book of recipe(1 month 1 book)</p>
              <p></p></a>
            </td>
@@ -72,7 +72,7 @@ include 'html_php/new_hearder.php';
              </p>
            </td>
            <td>
-             <a onClick="changeColour('b')"><h4>RM55</h4>
+             <a onClick="changeColour('b')"><h4>RM115</h4>
              <p>6 month of the book of recipe(1 month 1 book)</p>
              <p></p>
            </td></a>
@@ -85,7 +85,7 @@ include 'html_php/new_hearder.php';
              </p>
            </td>
            <td>
-             <a onClick="changeColour('c')"><h4>RM35</h4>
+             <a onClick="changeColour('c')"><h4>RM55</h4>
              <p>3 month of the book of recipe(1 month 1 book)</p>
              <p></p></a>
            </td>
@@ -120,8 +120,11 @@ include 'html_php/new_hearder.php';
                document.getElementById("third").style.backgroundColor = "white";
                document.getElementById("four").style.backgroundColor = "white";
                document.getElementById("first_radio").checked = true;
-               document.getElementById("price_text").innerHTML="RM100";
-               document.getElementById("price").value = "100";
+               document.getElementById("text_summary").innerHTML="1 year of the book of recipe(1 month 1 book)";
+               document.getElementById("book_price").innerHTML="RM20 * 12";
+               document.getElementById("price_text").innerHTML="RM235";
+               document.getElementById("price_text_2").innerHTML="RM235";
+               document.getElementById("price").value = "235";
            break;
            case 'b':
                color = "#D3D3D3";
@@ -130,8 +133,11 @@ include 'html_php/new_hearder.php';
                document.getElementById("third").style.backgroundColor = "white";
                document.getElementById("four").style.backgroundColor = "white";
                document.getElementById("second_radio").checked = true;
-               document.getElementById("price_text").innerHTML="RM55";
-               document.getElementById("price").value = "55";
+               document.getElementById("text_summary").innerHTML="6 month of the book of recipe(1 month 1 book)";
+               document.getElementById("price_text").innerHTML="RM115";
+               document.getElementById("price_text_2").innerHTML="RM115";
+               document.getElementById("book_price").innerHTML="RM20 * 6";
+               document.getElementById("price").value = "115";
            break;
            case 'c':
                color = "#D3D3D3";
@@ -140,8 +146,11 @@ include 'html_php/new_hearder.php';
                document.getElementById("third").style.backgroundColor = color;
                document.getElementById("four").style.backgroundColor = "white";
                document.getElementById("info").checked = true;
-               document.getElementById("price_text").innerHTML="RM35";
-               document.getElementById("price").value = "35";
+               document.getElementById("text_summary").innerHTML="3 month of the book of recipe(1 month 1 book)";
+               document.getElementById("price_text").innerHTML="RM55";
+               document.getElementById("price_text_2").innerHTML="RM55";
+               document.getElementById("book_price").innerHTML="RM20 * 3";
+               document.getElementById("price").value = "55";
            break;
            case 'd':
                color = "#D3D3D3";
@@ -150,7 +159,10 @@ include 'html_php/new_hearder.php';
                document.getElementById("third").style.backgroundColor = "white";
                document.getElementById("four").style.backgroundColor = color;
                document.getElementById("info1").checked = true;
+               document.getElementById("text_summary").innerHTML="1 month of the book of recipe(1 month 1 book)";
                document.getElementById("price_text").innerHTML="RM15";
+               document.getElementById("price_text_2").innerHTML="RM15";
+               document.getElementById("book_price").innerHTML="RM20 * 1";
                document.getElementById("price").value = "15";
            break;
        }
@@ -160,10 +172,25 @@ include 'html_php/new_hearder.php';
      </script>
 
      <span id="fooBar"></span>
-     <div class="col l3 m6 s12 blue" id="sticker">
+     <div class="col l3 m12 s12 grey lighten-4" id="sticker">
        <form onSubmit="return check_payment()" action="payments.php" method="post" id="paypal_form" target="_blank" class="paypal">
          <input type="hidden" name="cmd" value="_xclick"/>
-         <h3>Price: <b id="price_text"></b> </h3>
+         <h4>Booking summary</h4>
+         <p id="text_summary"></p>
+         <h4>Total Price: <b id="price_text"></b> </h4>
+         <hr>
+         <h5><b>Book price , teaxes and discount</b></h5>
+         <div class="white">
+           Book price  : <span class="right" id="book_price"></span>
+           <br>
+           GST (0%)    : <span class="right" id="book_price">RM0.00</span>
+           <br>
+           Discount    : <span class="right" id="book_price">RM5.00</span>
+           <br>
+           Total price : <b><span class="right" id="price_text_2"></span></b>
+         </div>
+         <hr>
+
          <input type="hidden" id="username" name="username" value="<?php echo $_SESSION['username']; ?>">
          <input type="hidden" required name="price" id="price" value="">
          <button  id="payment_button" class="waves-effect waves-light btn" style="width:100%" value="" onClick="button();">Process to checkout</button>
