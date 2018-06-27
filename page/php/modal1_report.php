bc9daaf4e81dbc8bae4a1c98901941065c6145c4
alan19031216 <terry1903@live.com>
2018-06-27 10:20:57 +0800

    add report history

--- page/php/modal1_report.php
+++ page/php/modal1_report.php
@@ -10,11 +10,14 @@ $myArray = explode(',', $check);
 //print_r($myArray);
 $a = implode(" <br>",$myArray);
 //echo $a;
+$code = substr(str_shuffle(str_repeat("0123456789ABCDEFGHIJKLMNOPQRSTUVWXZY", 5)), 0, 5);
 
 try {
-  $sql =$conn->query("INSERT INTO report (username , question_id , reason , reason_2) VALUES
-    ('$username' , '$product_id' , '$a' , '$modal1_question_TA')");
+  $sql =$conn->query("INSERT INTO report (username , question_id , code , reason , reason_2 , status) VALUES
+    ('$username' , '$product_id' , '$code' , '$a' , '$modal1_question_TA' , 'Reported')");
   if($sql){
+    $sql_notification = $conn->query("INSERT INTO report_history (username , code , status , admin_response) VALUES
+    ('$username' , '$code' , 'Reported' , '-')");
     echo "1";
   }
   else {
