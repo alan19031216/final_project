056a89fcf63087c2f071ce2214a025dedf82ddd4
alan19031216 <terry1903@live.com>
2018-06-27 16:49:51 +0800

    add report history

--- page/php/modal2_report.php
+++ page/php/modal2_report.php
@@ -9,11 +9,15 @@ $modal2_check = $_POST['modal2_check'];
 $myArray = explode(',', $modal2_check);
 //print_r($myArray);
 $a = implode(" <br>",$myArray);
+$code = substr(str_shuffle(str_repeat("0123456789ABCDEFGHIJKLMNOPQRSTUVWXZY", 5)), 0, 5);
 //echo $a;
 
 try {
   $sql =$conn->query("INSERT INTO report (username , comment_id , reason , reason_2) VALUES
     ('$username' , '$id' , '$a' , '$modal2_question_TA')");
+
+    $sql_notification = $conn->query("INSERT INTO report_history (username , code , status , admin_response) VALUES
+    ('$username' , '$code' , 'Reported' , '-')");
   if($sql){
     echo "1";
   }
