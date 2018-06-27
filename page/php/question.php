1a44eed451750195c21c3e848871afec485d9c37
alan19031216 <terry1903@live.com>
2018-06-27 16:16:12 +0800

    add path when insert success

--- page/php/question.php
+++ page/php/question.php
@@ -9,7 +9,11 @@ try {
 
   $sql = $conn->query("INSERT INTO question (username , title , description)
   VALUES ('$username' , '$title' , '$description')");
-  
+  echo "<script>
+        alert('Post success');
+        window.location.href='question.php';
+        </script>";
+
 } catch (PDOException $e) {
   print $e;
 }
