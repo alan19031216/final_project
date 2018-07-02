9031a700f326c1b9d8793379f17e78559b9e8190
alan19031216 <terry1903@live.com>
2018-07-02 21:32:18 +0800

    add google analytics api

--- new_index.php
+++ new_index.php
@@ -1,6 +1,15 @@
 <?php
 include 'header.php';
  ?>
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121742810-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-121742810-1');
+    </script>
 
     <div class="row hide-on-med-and-down" id="narbar">
       <div class="col l4 m4 s6">
@@ -287,6 +296,32 @@ include 'header.php';
             require 'html_php/content_html.php';
            ?>
         </div> <!--content END-->
+
+        <footer class="page-footer">
+               <div class="container">
+                 <div class="row">
+                   <div class="col l6 s12">
+                     <h5 class="white-text">Let's cook</h5>
+                     <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                   </div>
+                   <div class="col l4 offset-l2 s12">
+                     <h5 class="white-text">Links</h5>
+                     <ul>
+                       <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li> <br>
+                       <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li> <br>
+                       <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li> <br>
+                       <li><a class="grey-text text-lighten-3" href="admin/login.php">Admin</a></li>
+                     </ul>
+                   </div>
+                 </div>
+               </div>
+               <div class="footer-copyright">
+                 <div class="container">
+                 © 2018 Copyright Text
+                 <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                 </div>
+               </div>
+             </footer>
 
   </body>
 </html>
