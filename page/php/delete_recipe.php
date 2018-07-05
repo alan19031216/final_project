352431fc09803c7fe39cea602a943b812bb1b18f
alan19031216 <terry1903@live.com>
2018-07-05 15:03:50 +0800

    insert notification when user delete a recipe

--- page/php/delete_recipe.php
+++ page/php/delete_recipe.php
@@ -5,6 +5,7 @@ include 'config.php';
 $draft_id = $_POST['draft_id'];
 $sql_select = $conn->query("SELECT * FROM recipe WHERE id = '$draft_id'");
 foreach ($sql_select as $row) {
+  $name = $row['name'];
   $code = $row['code'];
 }
 
@@ -13,6 +14,12 @@ try {
   $sql_delete_i = $conn->query("DELETE FROM ingredients WHERE code = '$code'");
   $sql_delete_s = $conn->query("DELETE FROM food_step WHERE code = '$code'");
   if($sql_delete == true || $sql_delete_i == true || $sql_delete_s == true){
+    $sql_select_f = $conn->query("SELECT * FROM favorite WHERE code = '$code'");
+    foreach ($sql_select_f as $row_F) {
+      $username = $row_F['username'];
+      $sql_notification = $conn->query("INSERT INTO notification (username , title , reason) VALUES ('$username' ,  'Remove favorite' , '$name has been remove from your favorite because the author has remove it.')");
+    }
+    $sql_delete_f = $conn->query("DELETE FROM favorite WHERE code = '$code'");
     echo "1";
   }
   else{
