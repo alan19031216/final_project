54a28c06c7107c3d9b765aea8af69be7708e774b
alan19031216 <terry1903@live.com>
2018-07-05 20:45:00 +0800

    get user ip

--- page/new_home.php
+++ page/new_home.php
@@ -9,7 +9,22 @@
   if($image == "" || $image == " " || $image == "img/"){
     $image = "img/user_icon.png";
   }
- ?>
+
+   // gets the user IP Address
+   $user_ip=$_SERVER['REMOTE_ADDR'];
+
+   $check_ip = $conn->query("select userip from pageview where page='yourpage' and userip='$user_ip'");
+   $countRow = $check_ip->rowCount();
+   if($countRow>=1)
+   {
+   }
+   else
+   {
+     $insertview = $conn->query("insert into pageview values('','yourpage','$user_ip')");
+   }
+  ?>
+
+
  <style media="screen">
  .td , .td1{
      border-bottom: 1px solid #ddd;
