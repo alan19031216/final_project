57ffe51df2821d5cf7c26bd66fca71b480c40731
alan19031216 <terry1903@live.com>
2018-07-05 10:09:45 +0800

    add session for user

--- page/html_php/comment_row.php
+++ page/html_php/comment_row.php
@@ -1,18 +1,19 @@
 <div class="" id="comment_row">
   <?php
-    //echo $username ;
-    $img = "";
+    $username = $_SESSION['username'];
+    $img_read = "";
     $sql_user = $conn->query("SELECT * FROM user WHERE username = '$username'");
     foreach ($sql_user as $row) {
-      $img = $row['img'];
+      $img_read = $row['img'];
     }
-    if($img == "" || $img == " " || $img == "img/"){
-      $img = "img/user_icon.png";
+
+    if($img_read == "" || $img_read == " "){
+      $img_read = "img/user_icon.png";
     }
    ?>
   <div class="col l3 m3 s12">
-     <img src="<?php echo $img; ?>" alt="" class="circle responsive-img hide-on-small-only right" width="50%">
-     <center><img src="<?php echo $img; ?>" alt="" class="circle responsive-img hide-on-med-and-up " width="30%"></center>
+     <img src="<?php echo $img_read; ?>" alt="" class="circle responsive-img hide-on-small-only right" width="50%">
+     <center><img src="<?php echo $img_read; ?>" alt="" class="circle responsive-img hide-on-med-and-up " width="30%"></center>
 
   </div>
   <div class="col l9 m9 s12">
