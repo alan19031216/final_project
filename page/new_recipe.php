a169feea9767ff6bace4e6d04a33b37f483675c8
alan19031216 <terry1903@live.com>
2018-07-09 15:32:54 +0800

    add comment row

--- page/new_recipe.php
+++ page/new_recipe.php
@@ -774,9 +774,34 @@ $username = $_SESSION['username'];
            <div class="row">
              <input type="hidden" id="product_id_TA" value="<?php echo $_GET['code']; ?>">
              <input type="hidden" id="username_comment" name="" value="<?php echo $_SESSION['username']; ?>">
-             <?php
-               include 'html_php/comment_row.php';
-              ?>
+             <div class="" id="comment_row">
+               <?php
+                 //session_start();
+                 $username_comment = $_SESSION['username'];
+                 //echo $username_comment;
+                 $img_read = "";
+                 $sql_user = $conn->query("SELECT * FROM user WHERE username = '$username_comment'");
+                 foreach ($sql_user as $row) {
+                   $img_read = $row['img'];
+                 }
+                 //echo $img_read;
+                 if($img_read == "" || $img_read == " "){
+                   $img_read = "../page/php/img/user_image/user_icon.png";
+                 }
+                ?>
+               <div class="col l3 m3 s12">
+                  <img src="../<?php echo $img_read; ?>" alt="" class="circle responsive-img hide-on-small-only right" width="50%">
+                  <center><img src="<?php echo $img_read; ?>" alt="" class="circle responsive-img hide-on-med-and-up " width="30%"></center>
+               </div>
+
+               <div class="col l9 m9 s12">
+                 <form class="" method="post">
+                   <label for="textarea1">Comment</label>
+                   <textarea required class="browser-default" placeholder="Write your comment" name="description" id="comment_TA" class="materialize-textarea" style="width: 100%;height: 100px;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;"></textarea>
+                   <a class="waves-effect waves-light btn" id="" onclick="submit_comment()">Comment</a>
+                 </form>
+               </div>
+             </div><!-- comment_row -->
 
               <script type="text/javascript">
                 function submit_comment(){
