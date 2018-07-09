cae9de0627373827b3b4f43898c1cd39f4ff5ca6
alan19031216 <terry1903@live.com>
2018-07-09 16:25:50 +0800

    send a notification to user

--- admin/page/php/report.php
+++ admin/page/php/report.php
@@ -2,6 +2,7 @@
 require 'config.php';
 
 $report_username = $_POST['report_username'];
+$username = $_POST['username'];
 $code = $_POST['code'];
 $id = $_POST['id'];
 $txt = $_POST['txt'];
@@ -16,7 +17,7 @@ try {
   else{
     $sql = $conn->query("DELETE FROM question WHERE id = '$id'");
   }
-
+  $sql_notification = $conn->query("INSERT INTO notification (username , title , reason) VALUES ('$username' , 'Warning' , 'Please pay attention that the words you are using in comment.')");
   $sql_update_report = $conn->query("UPDATE report_history set status = 'Report success' , admin_response = 'This comment has been delete. Thank you report!' WHERE code = '$code'");
   $sql_report = $conn->query("DELETE FROM report WHERE code = '$code'");
   echo "1";
