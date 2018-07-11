0eea5a9cf87f61c08751a3607793a8553b0b9cd1
alan19031216 <terry1903@live.com>
2018-07-11 17:29:21 +0800

    add type and change path

--- admin/page/php/confirm_password.php
+++ admin/page/php/confirm_password.php
@@ -7,12 +7,12 @@ $salt = '}#f4ga~g%7hjg4&j(7mk?/!bj30ab-wi=6^7-$^R9F|GK5J#E6WT;IO[JN'; // random 
 $password = sha1(md5($salt.$password)); // sha1 hash with salt & md5 #6
 
 try {
-  $sql = $conn->query("UPDATE admin_login SET code = '' , password = '$password' WHERE code = '$code'");
+  $sql = $conn->query("UPDATE login SET code = '' , password = '$password' WHERE code = '$code'");
   if($sql){
     echo '<script language="javascript">';
     echo 'alert("Insert success")';
     echo '</script>';
-    header( "refresh:0.1; url=../../login.php" );
+    header( "refresh:0.1; url=../../../login_register.php" );
   }
 } catch(PDOException $e) {
   echo "Error: " . $e->getMessage();
