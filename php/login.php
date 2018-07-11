3fb320dbff32e2bf4acd4f2f0d1acf35e7187e74
alan19031216 <terry1903@live.com>
2018-07-11 17:20:01 +0800

    check type is user or admin

--- php/login.php
+++ php/login.php
@@ -10,10 +10,17 @@ $password = sha1(md5($salt.$password)); // sha1 hash with salt & md5 #6
 try {
     $stmt = $conn->prepare("SELECT * FROM login WHERE username = '$username' AND password = '$password' LIMIT 1");
     $stmt->execute();
+    foreach ($stmt as $row) {
+      $type = $row['type'];
+    }
     $count = $stmt->rowCount();
-    if($count > 0){
+    if($count == 1 & $type == 'u' ){
       $_SESSION['username'] = $username;
       header('Location: ../page/new_home.php');
+    }
+    elseif($count == 1 & $type == 'a' ){
+      $_SESSION['username'] = $username;
+      header('Location: ../admin/page/home.php');
     }
     else {
       echo '<script language="javascript">';
