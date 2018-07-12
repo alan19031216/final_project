21ad5ccb55b4988591003d99703e961395ca5c6a
alan19031216 <terry1903@live.com>
2018-07-12 23:21:16 +0800

    add select

---
+++ page/html_php/category_select.php
@@ -0,0 +1,12 @@
+<select class="browser-default" id="change1" onclick="select_onchange('1');">
+  <option disabled selected>Choose your option</option>
+  <?php
+     require 'php/config.php';
+     $sql_category = $conn->query("SELECT * FROM category");
+     foreach ($sql_category as $row_category) {
+ ?>
+   <option value="<?php echo $row_category['name']; ?>"><?php echo $row_category['name']; ?></option>
+ <?php
+   }
+ ?>
+<option value="Request">Request</option></select>
