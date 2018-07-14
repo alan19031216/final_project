cf723f44befc16fbbe2e516f08a4620c5bae4520
alan19031216 <terry1903@live.com>
2018-07-14 20:11:58 +0800

    ad  scroll down to load more

--- html_php/content_html.php
+++ html_php/content_html.php
@@ -1,188 +1,124 @@
+<style>
+  .demo-table {width: 100%;border-spacing: initial;margin: 10px 0px;word-break: break-word;table-layout: auto;line-height:4.8em;color:#333;}
+  .demo-table td {border-bottom: #f0f0f0 1px solid;background-color: #ffffff;padding: 5px;}
+  .demo-table ul{margin:0;padding:0;}
+  .demo-table li{cursor:pointer;list-style-type: none;display: inline-block;color: #F0F0F0;text-shadow: 0 0 1px #666666;font-size:23px;}
+  .demo-table .highlight, .demo-table .selected {color:#F4B30A;text-shadow: 0 0 1px #F48F0A;}
+</style>
+<div class="container row">
+  <h2 class="center">Recommendation</h2>
+  <script>
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            var position = $(window).scrollTop();
+            var bottom = $(document).height() - $(window).height();
+            //alert(position);
+            //alert(bottom);
+            if(position > (bottom - 5) ){
 
-<?php
-  require 'php/config.php';
-  $count_row = $conn->query("SELECT * FROM recipe");
-  $count = 0;
-    foreach ($count_row as $row) {
-      $count++;
-    }
- ?>
+                var row = Number($('#row').val());
+                var allcount = Number($('#all').val());
+                var rowperpage = 3;
+                row = row + rowperpage;
+                //alert(row);
+                if(row <= allcount){
+                  //alert(row);
+                    $('#row').val(row);
+                    $.ajax({
+                        url: 'php/fetch_data.php',
+                        type: 'post',
+                        data: {row:row},
+                        success: function(response){
+                            //alert(response);
+                            $('.parallax').parallax();
+                            $(".post:last").after(response).show().fadeIn("slow");
+                        },
+                        error: function(a){
+                          alert(a);
+                        }
+                    });
+                }
+            }
+        });
+    });
+    </script>
+    <?php
+      require 'php/config.php';
+      $rowperpage = 6;
+      $sql = $conn->query("SELECT * FROM recipe LIMIT 0 , $rowperpage");
+      $count_recipe = $sql->rowCount();
+      //echo $count_recipe;
+      foreach ($sql as $row_my_recipe) {
+        $code = $row_my_recipe['code'];
+    ?>
+  <div class="post" id="post_<?php echo $row_my_recipe['id']; ?>">
+    <div class="col l4 m6 s12">
+      <div class="card sticky-action card-shake hoverable" style="height:500px">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="page/<?php echo $row_my_recipe['cover_img'];?>" style="width:100%;height:200px;%;">
+        </div>
 
-<div class="row hide-on-small-only post" id="post_<?php echo $id; ?>">
-  <?php
-  $select = $conn->query("SELECT * FROM recipe LIMIT 4");
-  foreach ($select as $row) {
-    $code = $row['code'];
-   ?>
-  <div class="col l3 m6 s12">
-    <div class="card medium sticky-action" style="height:450px">
-      <div class="card-image waves-effect waves-block waves-light">
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4"><?php echo $row_my_recipe['name']; ?><i class="material-icons right">more_vert</i></span>
 
-        <img loop class="activator" src="../page/<?php  echo $row['cover_img'];?>">
-      </div>
+          <table class="demo-table">
+            <tbody>
+            <?php
+              $select_rate = $conn->query("SELECT * FROM tutorial WHERE code = '$code'");
+              foreach ($select_rate as $tutorial) {
+            ?>
+            <tr>
+              <td valign="top">
+                  <div>
+                    <ul >
+                      <?php
+                      for($i=1;$i<=5;$i++) {
+                      $selected = "";
+                      if(!empty($tutorial["rating"]) && $i<=$tutorial["rating"]) {
+                      $selected = "selected";
+                      }
+                      ?>
+                      <li class="<?php echo $selected; ?> hide-on-small-only" id="rate_view_<?php echo $i; ?>" style="font-size:20px">
+                         &#9733;
+                       </li>
+                       <li class="<?php echo $selected; ?> hide-on-med-and-up" id="rate_view_<?php echo $i; ?>" style="font-size:45px">
+                          &#9733;
+                        </li>
+                      <?php } // , <?php echo $tutorial['code'];  ?>
+                    </ul>
+                  </div>
+                </td>
+            </tr>
+            <?php
+              }
+            ?>
+            </tbody>
+          </table>
+        </div>
+        <div class="card-action">
+          Type: <?php echo $row_my_recipe['type']; ?>
+          <a class="btn-floating waves-effect waves-light red right btn tooltipped a-view_recipe" data-position="right" data-tooltip="View Recipe" href="recipe/<?php echo $row_my_recipe['code']; ?>"><i class="material-icons">book</i></a>
+          <!-- <a id="<?php echo $row_my_recipe['code']; ?>" class="btn-floating waves-effect waves-light right tooltipped"  data-position="top" data-tooltip="Add to favorite" onclick="addFavorite('<?php echo $row_my_recipe['code']; ?>')"><i class="material-icons">stars</i></a> -->
+          <br><br>
+        </div>
 
-      <div class="card-content">
-        <span class="card-title activator grey-text text-darken-4"><?php echo $row['name']; ?><i class="material-icons right">more_vert</i></span>
-
-        <style>
-           ul{margin:0;padding:0;}
-           li{cursor:pointer;list-style-type: none;display: inline-block;color: #F0F0F0;text-shadow: 0 0 1px #666666;font-size:25px;}
-           .highlight, .selected {color:#F4B30A;text-shadow: 0 0 1px #F48F0A;}
-        </style>
-
-        <?php
-          $select_rate = $conn->query("SELECT * FROM tutorial WHERE code = '$code'");
-          foreach ($select_rate as $tutorial) {
-        ?>
-
-              <div id="tutorial-<?php echo $tutorial["id"]; ?>">
-                <ul onMouseOut="resetRating(<?php echo $tutorial["id"]; ?>);">
-                  <?php
-                  for($i=1;$i<=5;$i++) {
-                  $selected = "";
-                  if(!empty($tutorial["rating"]) && $i<=$tutorial["rating"]) {
-                  $selected = "selected";
-                  }
-                  ?>
-                  <li class='<?php echo $selected; ?>'>
-                     &#9733;
-                   </li>
-                  <?php
-                    } // , <?php echo $tutorial['code'];  ?>
-                </ul>
-                </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4"><?php echo $row_my_recipe['name']; ?><i class="material-icons right">close</i></span>
           <?php
+            $simple_description = $row_my_recipe['simple_description'];
+
+            if($simple_description == '' || $simple_description == ' '){
+              $simple_description = 'He/She is very lazy...Nothings to show';
             }
           ?>
+          <p> <?php echo $simple_description ?> </p>
+        </div>
       </div>
-
-      <div class="card-action">
-        Type: <?php echo $row['type']; ?>
-        <a class="btn-floating waves-effect waves-light red right" href="recipe/<?php echo $row['code']; ?>"><i class="material-icons">book</i></a>
       </div>
-
-
-      <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4"><?php echo $row['name']; ?><i class="material-icons right">close</i></span>
-        <!--<p>author: <?php //echo $row['usern']; ?></p>-->
-        <?php
-          $simple_description = $row['simple_description'];
-          if($simple_description == '' || $simple_description == ' '){
-            $simple_description = 'He/She is very lazy...Nothings to show';
-          }
-        ?>
-        <p> <?php echo $simple_description ?> </p>
-      </div>
-    </div>
-  </div> <!--div col s3 END-->
-  <?php
-    } // foreach END
-   ?>
-   <br><br>
-   <h4 class="center-align">You may also like</h4>
-   <br>
-   <div class="">
-     <table>
-       <?php
-       $count_row = $conn->query("SELECT * FROM recipe");
-       $allcount = $count_row->rowCount();
-       //echo $allcount;
-       $temp = $count/4;
-       $total = ceil($temp);
-       if($total > 5){
-         $total = 4;
-       }
-       //echo $total;
-       $a = 1;
-       $b = 5;
-       $c = 0;
-        for($i = 0; $i < $total; $i++){
-          //echo $c;
-        ?>
-       <tr>
-         <?php
-         $select_all = $conn->query("SELECT * FROM recipe LIMIT $a , $b");
-         foreach ($select_all as $row) {
-           $name = $row['name'];
-           print '<td>';
-           ?>
-           <div class="">
-             <div class="card medium sticky-action">
-               <div class="card-image waves-effect waves-block waves-light">
-                 <img loop class="activator" src="page/php/<?php  echo $row['cover_img'];?>">
-               </div>
-
-               <div class="card-content">
-                 <span class="card-title activator grey-text text-darken-4"><?php echo $row['name']; ?><i class="material-icons right">more_vert</i></span>
-                 <?php
-                   $select_rate1 = $conn->query("SELECT * FROM tutorial WHERE code = '$code'");
-                   foreach ($select_rate1 as $tutorial1) {
-                 ?>
-
-                       <div id="tutorial-<?php echo $tutorial1["id"]; ?>">
-                         <ul onMouseOut="resetRating(<?php echo $tutorial1["id"]; ?>);">
-                           <?php
-                           for($i=1;$i<=5;$i++) {
-                           $selected = "";
-                           if(!empty($tutorial1["rating"]) && $i<=$tutorial1["rating"]) {
-                           $selected = "selected";
-                           }
-                           ?>
-                           <li class='<?php echo $selected; ?>'>
-                              &#9733;
-                            </li>
-                           <?php
-                             } // , <?php echo $tutorial['code'];  ?>
-                         </ul>
-                         </div>
-                   <?php
-                     }
-                   ?>
-
-               </div>
-
-               <div class="card-action">
-                 Type: <?php echo $row['type']; ?>
-                 <a class="btn-floating waves-effect waves-light red right" href="recipe.php?code=<?php echo $row['code']; ?>"><i class="material-icons">book</i></a>
-               </div>
-
-
-               <div class="card-reveal">
-                 <span class="card-title grey-text text-darken-4"><?php echo $row['name']; ?><i class="material-icons right">close</i></span>
-                 <!--<p>author: <?php //echo $row['usern']; ?></p>-->
-                 <?php
-                   $simple_description = $row['simple_description'];
-                   if($simple_description == '' || $simple_description == ' '){
-                     $simple_description = 'He/She is very lazy...Nothings to show';
-                   }
-                 ?>
-                 <p> <?php echo $simple_description ?> </p>
-               </div>
-             </div>
-           </div> <!--div col s3 END-->
-           <?php
-           print '</td>';
-           //echo $c++;
-         }
-         $a+=5;
-          ?>
-       </tr>
-      <?php } ?>
-     </table>
-   </div>
-</div> <!--row END-->
-<center>
-  <div class="hide-on-small-only">
-    <h1 class="load-more">Load More</h1>
-    <input type="hidden" id="row" value="0">
-    <input type="hidden" id="all" value="<?php echo $allcount; ?>">
-  </div>
-
-</center>
-
-
-<div class="hide-on-med-and-up show-on-small">
-  <?php
-    include 'html_php/content_html_phone.php';
-   ?>
-</div>
+    </div> <!--div col s3 END-->
+    <?php
+      }
+     ?>
+     <input type="hidden" id="row" value="0">
+     <input type="hidden" id="all" value="<?php echo $count_recipe; ?>">
+</div> <!-- row -->
--- page/new_home.php
+++ page/new_home.php
@@ -134,9 +134,141 @@
    </div>
  </div>
 
- <hr>
- <h2>Recommendation</h2>
- <br><br><br><br>
+ <div class="parallax-container" style="height:300px">
+   <div class="parallax"><img src="img/home_parallax.jpg"></div>
+ </div>
+
+ <script type="text/javascript">
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+ </script>
+
+ <style>
+   .demo-table {width: 100%;border-spacing: initial;margin: 10px 0px;word-break: break-word;table-layout: auto;line-height:4.8em;color:#333;}
+   .demo-table td {border-bottom: #f0f0f0 1px solid;background-color: #ffffff;padding: 5px;}
+   .demo-table ul{margin:0;padding:0;}
+   .demo-table li{cursor:pointer;list-style-type: none;display: inline-block;color: #F0F0F0;text-shadow: 0 0 1px #666666;font-size:23px;}
+   .demo-table .highlight, .demo-table .selected {color:#F4B30A;text-shadow: 0 0 1px #F48F0A;}
+ </style>
+
+    <div class="container row">
+      <h2 class="center">Recommendation</h2>
+      <script>
+        $(document).ready(function(){
+            $(window).scroll(function(){
+                var position = $(window).scrollTop();
+                var bottom = $(document).height() - $(window).height();
+                //alert(position);
+                //alert(bottom);
+                if(position > (bottom - 5) ){
+
+                    var row = Number($('#row').val());
+                    var allcount = Number($('#all').val());
+                    var rowperpage = 3;
+                    row = row + rowperpage;
+                    //alert(row);
+                    if(row <= allcount){
+                      //alert(row);
+                        $('#row').val(row);
+                        $.ajax({
+                            url: 'php/fetch_data.php',
+                            type: 'post',
+                            data: {row:row},
+                            success: function(response){
+                                //alert(response);
+                                $('.parallax').parallax();
+                                $(".post:last").after(response).show().fadeIn("slow");
+                            },
+                            error: function(a){
+                              alert(a);
+                            }
+                        });
+                    }
+                }
+            });
+        });
+        </script>
+        <?php
+          require 'php/config.php';
+          $rowperpage = 6;
+          $sql = $conn->query("SELECT * FROM recipe LIMIT 0 , $rowperpage");
+          $count_recipe = $sql->rowCount();
+          //echo $count_recipe;
+          foreach ($sql as $row_my_recipe) {
+            $code = $row_my_recipe['code'];
+        ?>
+      <div class="post" id="post_<?php echo $row_my_recipe['id']; ?>">
+        <div class="col l4 m6 s12">
+          <div class="card sticky-action card-shake hoverable" style="height:500px">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="<?php echo $row_my_recipe['cover_img'];?>" style="width:100%;height:200px;%;">
+            </div>
+
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4"><?php echo $row_my_recipe['name']; ?><i class="material-icons right">more_vert</i></span>
+
+              <table class="demo-table">
+                <tbody>
+                <?php
+                  $select_rate = $conn->query("SELECT * FROM tutorial WHERE code = '$code'");
+                  foreach ($select_rate as $tutorial) {
+                ?>
+                <tr>
+                  <td valign="top">
+                      <div>
+                        <ul >
+                          <?php
+                          for($i=1;$i<=5;$i++) {
+                          $selected = "";
+                          if(!empty($tutorial["rating"]) && $i<=$tutorial["rating"]) {
+                          $selected = "selected";
+                          }
+                          ?>
+                          <li class="<?php echo $selected; ?> hide-on-small-only" id="rate_view_<?php echo $i; ?>" style="font-size:20px">
+                             &#9733;
+                           </li>
+                           <li class="<?php echo $selected; ?> hide-on-med-and-up" id="rate_view_<?php echo $i; ?>" style="font-size:45px">
+                              &#9733;
+                            </li>
+                          <?php } // , <?php echo $tutorial['code'];  ?>
+                        </ul>
+                      </div>
+                    </td>
+                </tr>
+                <?php
+                  }
+                ?>
+                </tbody>
+              </table>
+            </div>
+            <div class="card-action">
+              Type: <?php echo $row_my_recipe['type']; ?>
+              <a class="btn-floating waves-effect waves-light red right btn tooltipped a-view_recipe" data-position="right" data-tooltip="View Recipe" href="recipe/<?php echo $row_my_recipe['code']; ?>"><i class="material-icons">book</i></a>
+              <!-- <a id="<?php echo $row_my_recipe['code']; ?>" class="btn-floating waves-effect waves-light right tooltipped"  data-position="top" data-tooltip="Add to favorite" onclick="addFavorite('<?php echo $row_my_recipe['code']; ?>')"><i class="material-icons">stars</i></a> -->
+              <br><br>
+            </div>
+
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4"><?php echo $row_my_recipe['name']; ?><i class="material-icons right">close</i></span>
+              <?php
+                $simple_description = $row_my_recipe['simple_description'];
+
+                if($simple_description == '' || $simple_description == ' '){
+                  $simple_description = 'He/She is very lazy...Nothings to show';
+                }
+              ?>
+              <p> <?php echo $simple_description ?> </p>
+            </div>
+          </div>
+          </div>
+        </div> <!--div col s3 END-->
+        <?php
+          }
+         ?>
+         <input type="hidden" id="row" value="0">
+         <input type="hidden" id="all" value="<?php echo $count_recipe; ?>">
+    </div> <!-- row -->
 
    </body>
  </html>
