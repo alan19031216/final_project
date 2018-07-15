4afce82111bf996cff8dea99f670fb76dd562a63
alan19031216 <terry1903@live.com>
2018-07-15 20:35:53 +0800

    edit footer

--- html_php/footer.php
+++ html_php/footer.php
@@ -1,9 +1,33 @@
-
-
-</body>
-
-<script type="text/javascript">
-    // side nav (mobile)
-    $(".button-collapse").sideNav();
-</script>
-</html>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<footer class="page-footer orange">
+ <div class="container">
+   <div class="row">
+     <div class="col l6 s12">
+       <h5 class="white-text">Let's cook</h5>
+       <p class="grey-text text-lighten-4">Share what you want and get what you want!!</p>
+     </div>
+     <div class="col l4 offset-l2 s12">
+       <h5 class="white-text">Links</h5>
+       <ul>
+         <li><a class="grey-text text-lighten-3" href="#!">Privacy Policy</a></li>
+         <li><a class="grey-text text-lighten-3" href="#!">Contact Us</a></li>
+         <li><a class="grey-text text-lighten-3" href="#!">Follow Us</a></li>
+         <li>
+           <a class="grey-text text-lighten-3" href="#!">
+             <i class="fa fa-facebook-square" style="font-size:36px"></i>
+             <i class="fa fa-twitter-square" style="font-size:36px"></i>
+             <i class="fa fa-google-plus-square" style="font-size:36px"></i>
+             <i class="fa fa-youtube-square" style="font-size:36px"></i>
+           </a>
+         </li>
+       </ul>
+     </div>
+   </div>
+ </div>
+ <div class="footer-copyright">
+   <div class="container">
+   © 2018 Copyright Text
+   <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+   </div>
+ </div>
+</footer>
