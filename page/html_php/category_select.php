a97f269c9d94e1c15fdbcdb124e813fbf9c5e759
alan19031216 <terry1903@live.com>
2018-07-15 15:35:46 +0800

    add order by name

--- page/html_php/category_select.php
+++ page/html_php/category_select.php
@@ -2,7 +2,7 @@
   <option disabled selected>Choose your option</option>
   <?php
      require 'php/config.php';
-     $sql_category = $conn->query("SELECT * FROM category");
+     $sql_category = $conn->query("SELECT * FROM category ORDER BY name ASC");
      foreach ($sql_category as $row_category) {
  ?>
    <option value="<?php echo $row_category['name']; ?>"><?php echo $row_category['name']; ?></option>
