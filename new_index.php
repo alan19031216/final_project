d8d90106d19f289343f14e9c4c8935af5696e253
alan19031216 <terry1903@live.com>
2018-07-15 20:36:13 +0800

    add footer.php file

--- new_index.php
+++ new_index.php
@@ -292,34 +292,9 @@ include 'header.php';
         <div class="content">
           <?php
             require 'html_php/content_html.php';
+            require 'html_php/footer.php';
            ?>
         </div> <!--content END-->
-
-        <footer class="page-footer">
-               <div class="container">
-                 <div class="row">
-                   <div class="col l6 s12">
-                     <h5 class="white-text">Let's cook</h5>
-                     <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-                   </div>
-                   <div class="col l4 offset-l2 s12">
-                     <h5 class="white-text">Links</h5>
-                     <ul>
-                       <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li> <br>
-                       <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li> <br>
-                       <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li> <br>
-                       <li><a class="grey-text text-lighten-3" href="admin/login.php">Admin</a></li>
-                     </ul>
-                   </div>
-                 </div>
-               </div>
-               <div class="footer-copyright">
-                 <div class="container">
-                 © 2018 Copyright Text
-                 <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-                 </div>
-               </div>
-             </footer>
 
   </body>
 </html>
