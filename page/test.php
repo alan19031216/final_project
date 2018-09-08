2e32e1c6751fe9b77e4336fe08f711b23be75d3b
alan19031216 <terry1903@live.com>
2018-09-08 22:08:07 +0800

    new texteditor

--- page/test.php
+++ page/test.php
@@ -1,7 +1,17 @@
-<script type="text/javascript" src="js/nicEdit.js"></script>
-<script type="text/javascript">
-	bkLib.onDomLoaded(function() {
-		nicEditors.allTextAreas()
-	});
-</script>
-<textarea name="area3" style="width: 100%; height: 200px;"></textarea>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+<link href="css/summernote-bs4.css" rel="stylesheet">
+<script src="js/summernote-bs4.js"></script>
+
+  <textarea id="summernote" name="editordata"></textarea>
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#summernote').summernote({
+				placeholder: 'Your comment',
+				height: 250
+			});
+		});
+	</script>
