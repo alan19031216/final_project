f06e7c5a9008327f7136e7dfde6ae229618d2d68
alan19031216 <terry1903@live.com>
2018-09-08 16:32:08 +0800

    change path

--- page/php/question.php
+++ page/php/question.php
@@ -9,10 +9,19 @@ try {
 
   $sql = $conn->query("INSERT INTO question (username , title , description)
   VALUES ('$username' , '$title' , '$description')");
-  echo "<script>
-        alert('Post success');
-        window.location.href='question.php';
-        </script>";
+  if ($sql) {
+    echo "<script>
+          alert('Post success');
+          window.location.href='../question.php';
+          </script>";
+  }
+  else{
+    echo "<script>
+          alert('Post fail');
+          window.location.href='../new_home.php';
+          </script>";
+  }
+
 
 } catch (PDOException $e) {
   print $e;
