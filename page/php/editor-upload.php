8e0c19c3e053729703618427627f303bbc5c06cf
alan19031216 <terry1903@live.com>
2018-09-13 18:36:19 +0800

    text editor for use answer a question

---
+++ page/php/editor-upload.php
@@ -0,0 +1,19 @@
+<?php
+if(empty($_FILES['file']))
+{
+    exit();
+}
+
+//include 'config.php';
+$errorImgFile = "img/img_upload_error.jpg";
+$temp = explode(".", $_FILES["file"]["name"]);
+$newfilename = round(microtime(true)) . '.' . end($temp);
+$destinationFilePath = 'img/'.$newfilename ;
+if(!move_uploaded_file($_FILES['file']['tmp_name'], $destinationFilePath)){
+    echo $errorImgFile;
+}
+else{
+    echo $destinationFilePath;
+}
+
+?>
