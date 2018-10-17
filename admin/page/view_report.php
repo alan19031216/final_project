a19160eba80830c3ddb6ca13bfaafcc697d49351
alan19031216 <terry1903@live.com>
2018-10-17 11:42:40 +0800

    include all li item

--- admin/page/view_report.php
+++ admin/page/view_report.php
@@ -26,33 +26,7 @@ if($username == '' || $username == ' '){
 
   <body>
     <ul id="slide-out" class="side-nav fixed z-depth-4">
-      <li>
-        <div class="userView">
-          <div class="background">
-            <img src="img/background.jpg">
-          </div>
-          <a href="#!user"><img class="circle" src="php/img/admin_icon.png"></a>
-          <a href="#!name"><span class="white-text name">Welcome back, <?php echo $username ?></span></a>
-        </div>
-      </li>
-
-      <li>
-        <form class="sidebar-form">
-          <div class="input-group">
-            <input id="accounts" type="text" name="username" class="form-control" placeholder="Universal Search" autocomplete="off" />
-          </div>
-        </form>
-      </li>
-
-      <li><a class="active" href="home.php"><i class="material-icons pink-item">dashboard</i>Dashboard</a></li>
-      <li><div class="divider"></div></li>
-      <li><a class="subheader">Management</a></li>
-      <li><a href="#" class="add_admin_b"><i class="material-icons pink-item">person_add</i>Add Admin</a></li>
-      <li><a href="#" class="pull_book"><i class="material-icons pink-item">file_upload</i>Pull book</a></li>
-      <li><a href="#" class="request"><i class="material-icons pink-item">gavel</i>Request</a></li>
-      <li><a href="view_report.php" class="view_report"><i class="material-icons pink-item">report_problem</i>View report</a></li>
-      <li><a href="../../page/new_home.php" class=""><i class="material-icons pink-item">pageview</i>Page view</a></li>
-      <li><a href="logout.php"><i class="material-icons pink-item">remove_circle_outline</i>Logout</a></li>
+      <?php include 'li_item.php'; ?>
     </ul>
     <script type="text/javascript">
     // clicking the edit button
