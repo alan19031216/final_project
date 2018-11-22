446d0540622bcf864c80fc393f719b7961ad4050
alan19031216 <terry1903@live.com>
2018-11-22 19:40:28 +0800

    insert title

--- login_register.php
+++ login_register.php
@@ -8,7 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src='https://www.google.com/recaptcha/api.js'></script>
     <script type="text/javascript" src="../js/materialize.min.js"></script>
-    <title></title>
+    <title>Login and Register</title>
   </head>
   <body>
     <div class="component">
